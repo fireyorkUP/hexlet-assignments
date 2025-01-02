dc2ccb83d9f38cf36a9eeafe11848395c0eb8e75
@hexlet/cli <support@hexlet.io>
2025-01-02 19:29:20 +0300

    submit java-spring-boot-ru/dto

---
+++ java-spring-boot-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,64 @@
+package exercise.controller;
+
+import exercise.model.Comment;
+import exercise.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.exception.ResourceNotFoundException;
+import exercise.dto.PostDTO;
+import exercise.dto.CommentDTO;
+
+// BEGIN
+@RestController
+@RequestMapping("posts")
+public class PostsController {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    private CommentDTO toCommentDTO(Comment comment) {
+        var dto = new CommentDTO();
+        dto.setId(comment.getId());
+        dto.setBody(comment.getBody());
+        return dto;
+    }
+
+    private PostDTO toPostDTO(Post post) {
+        var dto = new PostDTO();
+        dto.setId(post.getId());
+        dto.setTitle(post.getTitle());
+        dto.setBody(post.getBody());
+        var comments = commentRepository.findByPostId(post.getId());
+        dto.setComments(comments.stream()
+                .map(this::toCommentDTO)
+                .toList());
+        return dto;
+    }
+
+    @GetMapping(path = "")
+    public List<PostDTO> index() {
+        var posts = postRepository.findAll();
+        var result = posts.stream()
+                .map(this::toPostDTO)
+                .toList();
+        return result;
+    }
+
+    @GetMapping(path = "/{id}")
+    public PostDTO show(@PathVariable long id) {
+        var post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+        return toPostDTO(post);
+    }
+}
+// END

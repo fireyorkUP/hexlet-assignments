a3f1181e642d82f38dc056391626b2644d83c997
@hexlet/cli <support@hexlet.io>
2025-01-28 22:27:56 +0300

    submit java-advanced-ru/multithreading-java

---
+++ java-advanced-ru/multithreading-java/src/main/java/exercise/App.java
@@ -0,0 +1,37 @@
+package exercise;
+
+import java.util.Map;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+class App {
+    private static final Logger LOGGER = Logger.getLogger("AppLogger");
+
+    // BEGIN
+    public static Map<String, Integer> getMinMax(int[] array){
+
+        MinThread minThread = new MinThread(array);
+        MaxThread maxThread = new MaxThread(array);
+        minThread.start();
+        LOGGER.log(Level.INFO, "Thread " + minThread.getName() + " started");
+        maxThread.start();
+        LOGGER.log(Level.INFO, "Thread " + maxThread.getName() + " started");
+
+        try {
+            minThread.join();
+            LOGGER.log(Level.INFO, "Thread " + minThread.getName() + " finished");
+            maxThread.join();
+            LOGGER.log(Level.INFO, "Thread " + maxThread.getName() + " finished");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        Map<String, Integer> result = Map.of(
+                "min", minThread.getMin(),
+                "max", maxThread.getMax()
+        );
+        LOGGER.log(Level.INFO, "Result: " + result);
+        return result;
+    }
+    // END
+}
